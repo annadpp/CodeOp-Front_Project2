4fb600e4aeec4cbbc29dcc901914c4bb8de3bc7f
annadpp <anna.depablo.puig@gmail.com>
2023-07-18 20:41:15 +0200

    HTML 1.0 + CSS 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./input.css", "./input.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#1A2A33',
+        'middle': '#1F3540',
+        'turquoise': '#30C3BF',
+        'yellow': '#F1B236',
+        'light': '#A9BDC9'
+      }
+    },
   },
   plugins: [],
 };
