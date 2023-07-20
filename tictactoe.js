5ae7d08ce0112b3af7be978bca2ce42ede79fd0b
Anna de Pablo Puig <71819431+annadpp@users.noreply.github.com>
2023-07-20 21:45:14 +0200

    Js - check for draw/win

--- tictactoe.js
+++ tictactoe.js
@@ -4,18 +4,18 @@ const game = {
     oState: [],
     winningCombs: [
         // Rows
-        ['0', '1', '2'],
-        ['3', '4', '5'],
-        ['6', '7', '8'],
+        ["0", "1", "2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"],
 
         // Columns
-        ['0', '3', '6'],
-        ['1', '4', '7'],
-        ['2', '5', '8'],
+        ["0", "3", "6"],
+        ["1", "4", "7"],
+        ["2", "5", "8"],
 
         // Diagonal
-        ['0', '4', '8'],
-        ['2', '4', '6']
+        ["0", "4", "8"],
+        ["2", "4", "6"]
     ]
 }
 /////////////* // MENU // */////////////
@@ -25,38 +25,75 @@ const game = {
 
 const cells = document.querySelectorAll("[data-cell]");
 
-function setupCell(cells, game) {
+const gameOverText = document.getElementById("game-over-text");
+const firstParagraph = gameOverText.querySelector("p:first-child");
+const secondParagraph = gameOverText.querySelector("p:nth-child(2)");
 
-   // X / 0 stay on click
+function checkGameOver() {
+
+    //Checks if there are any playable cells left in the game -> means draw
+    if (!document.querySelectorAll("[data-cell]:not(.disabled)").length) {
+      document.getElementById("game-over").classList.remove("hidden");
+      firstParagraph.textContent = "The game of Schrödinger...";
+        secondParagraph.textContent = "Draw!";
+    }
+
+    //Check for winning combinations comparing xState/oState arrays with game.winningCombs
+    game.winningCombs.forEach(winningComb => {
+      const xWins = winningComb.every(state => game.xState.includes(state));
+      const oWins = winningComb.every(state => game.oState.includes(state));
+
+      if (xWins || oWins) {
+        document.querySelectorAll("[data-cell]").forEach(cell => cell.classList.add("disabled"));
+        document.getElementById("game-over").classList.remove("hidden");
+
+        if (xWins) {
+        firstParagraph.textContent = "You win!";
+        secondParagraph.textContent = "X wins";
+        } else {
+        firstParagraph.textContent = "You lose";
+        secondParagraph.textContent = "Y wins";
+        }
+
+        //Make background dark
+        //Change winning combination bg color
+      }
+    });
+  }
+
+function setupCell(cells, game) {
   cells.forEach(cell => {
-    cell.addEventListener('click', e => {
+
+    //Add X/O on click
+    cell.addEventListener("click", e => {
       const target = e.target;
       const cellValue = target.dataset.value;
 
       if (!target.classList.contains("disabled")) {
         target.classList.add("disabled");
         game.xTurn ? game.xState.push(cellValue) : game.oState.push(cellValue);
-        target.classList.add(game.xTurn ? 'x' : 'o');
+        target.classList.add(game.xTurn ? "x" : "o");
         game.xTurn = !game.xTurn;
+
+        checkGameOver(); // Call the function to check for game over condition after every click
       }
     });
 
-    // X / O appear on hover
-    cell.addEventListener('mouseover', e => {
+    //Add X/O on hover
+    cell.addEventListener("mouseover", e => {
       const target = e.target;
       if (!target.classList.contains("disabled")) {
-        target.classList.add(game.xTurn ? 'x' : 'o');
+        target.classList.add(game.xTurn ? "x" : "o");
       }
     });
 
-    cell.addEventListener('mouseout', e => {
+    cell.addEventListener("mouseout", e => {
       const target = e.target;
       if (!target.classList.contains("disabled")) {
-        target.classList.remove('x', 'o');
+        target.classList.remove("x", "o");
       }
     });
   });
-  
 
 }
 
