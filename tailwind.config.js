e657ca329a9005adfbf347fd75576b1025b37116
Anna de Pablo Puig <71819431+annadpp@users.noreply.github.com>
2023-07-27 21:35:03 +0200

    Js - vs CPU / restart / exit + HTML/CSS 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./input.css", "./input.js"],
+  content: ["index.html", "input.css", "tictactoe.js"],
   theme: {
     extend: {
       colors: {
         'dark': '#1A2A33',
+        'darkest': '#0b131a',
         'middle': '#1F3540',
         'turquoise': '#30C3BF',
         'yellow': '#F1B236',
